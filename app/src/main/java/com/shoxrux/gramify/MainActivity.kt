b5341fbd7b6134a63c9adc8ae5859fa9ae4f1c2c
Shoxrux061 <shoxruxurmanov2@gmail.com>
2025-02-25 16:06:08 +0500

    BottomNavigation completed

--- app/src/main/java/com/shoxrux/gramify/MainActivity.kt
+++ app/src/main/java/com/shoxrux/gramify/MainActivity.kt
@@ -3,7 +3,6 @@ package com.shoxrux.gramify
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.activity.enableEdgeToEdge
 import androidx.navigation.compose.rememberNavController
 import com.google.firebase.FirebaseApp
 import com.shoxrux.gramify.ui.AppNavHost
